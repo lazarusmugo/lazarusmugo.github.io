567c5a783fcdd5c8d2be1ce53326b506ab32687c
Lazarus Mugo <mugolazarusk@gmail.com>
2024-07-23 22:11:51 +0300

    Updates UI

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,20 +1,20 @@
 "use client";
 import React from "react";
-import { useRouter } from "next/router"; // Corrected import for useRouter
+// import { useRouter } from "next/router"; // Corrected import for useRouter
 
 const ProjectPage = ({ params }) => {
-  const router = useRouter();
+//   const router = useRouter();
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center absolute top-4 left-4 z-50">
-        <button
+        {/* <button
           onClick={() => router.back()}
           className="relative inline-block px-20 py-5 mt-6 text-custom-blue border border-custom-blue hover:text-white font-bold overflow-hidden group"
         >
           <span className="absolute inset-0 bg-custom-blue transform -translate-x-full group-hover:translate-x-0 transition-transform duration-300 ease-in-out"></span>
           <span className="relative">Go Back</span>
-        </button>
+        </button> */}
       </div>
     </div>
   );
